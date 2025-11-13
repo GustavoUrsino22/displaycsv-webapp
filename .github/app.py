576137fb640f0ef0ee00ae064c3b8c2635be9606
Gustavo Henrique (Ursino) <163691333+GustavoUrsino22@users.noreply.github.com>
2025-11-13 20:51:03 -0300

    Update app.py código

--- .github/app.py
+++ .github/app.py
@@ -1,1 +1,17 @@
+import pandas as pd
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    df = pd.read_csv("ai_job_trends_dataset.csv", sep=",")
+    return render_template(
+        "display.html",
+        tables=[df.to_html(classes="data")],
+        titles=df.columns.values
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
